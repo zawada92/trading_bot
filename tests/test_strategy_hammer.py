022f1c014a3c439375337d9fbf78274406ad70fa
Tomasz Zawadzki <tomek.zawadzki92@gmail.com>
2022-05-27 17:33:47 +0200

    Update test_strategy_hammer.py to new StrategyHammer implementation.

--- tests/test_strategy_hammer.py
+++ tests/test_strategy_hammer.py
@@ -10,6 +10,7 @@ from pandas import DataFrame
 
 from trading_bot.gateio_utils import ExchangeApi, Interval
 from trading_bot.indicators import Indicators
+from trading_bot.live_data import LiveData
 from trading_bot.strategy_hammer import StrategyHammer
 
 
@@ -19,19 +20,21 @@ class TestStrategyHammer(unittest.TestCase):
         """"Startegy hammer example 25th march at 9 on 1h chart."""
         self.exchange_api = ExchangeApi()
         self.contract = "BTC_USDT"
-        self.df = DataFrame(
-                    self.exchange_api.get_candle_stick_time_range(
-                        contract=self.contract,
-                        interval=Interval.INT_1H,
-                        start=datetime(2022, 3, 20, 10, 0),
-                        end=datetime(2022, 3, 25, 9, 0)
-                    ))
+        self.live_data = LiveData()
+        
+        self.live_data.df = DataFrame(
+                            self.exchange_api.get_candle_stick_time_range(
+                                contract=self.contract,
+                                interval=Interval.INT_1H,
+                                start=datetime(2022, 3, 20, 10, 0),
+                                end=datetime(2022, 3, 25, 9, 0)
+                            ))
         indicators = Indicators()
-        indicators.add_atr(self.df)
+        indicators.add_atr(self.live_data.df)
         # Add EMA 20 for hammer strategy
-        indicators.add_ema(self.df, 20)
+        indicators.add_ema(self.live_data.df, 20)
 
-        strategy_hammer = StrategyHammer(self.df)
+        strategy_hammer = StrategyHammer(self.live_data)
         strategy_hammer.start()
 
         self.assertTrue(strategy_hammer.is_setup_ready())
